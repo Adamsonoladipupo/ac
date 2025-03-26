f66bd8fb60546d9bb7ce7cc1326aad7c3b145d45
OlaAdams <75829183+Adamsonoladipupo@users.noreply.github.com>
2025-03-26 16:05:34 +0100

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -224,11 +224,11 @@ function create_sidebar () {
 
     //2c. add Logout links to the sidebar footer section
     //2c.. Define Logout SVGs
-    const logour_icon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 3h6a3 3 0 0 1 3 3v4h-1V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-4h1v4a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3m3 9h11.25L16 8.75l.66-.75l4.5 4.5l-4.5 4.5l-.66-.75L19.25 13H8z"/></svg>`;
+    const logout_icon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M5 3h6a3 3 0 0 1 3 3v4h-1V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2v-4h1v4a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V6a3 3 0 0 1 3-3m3 9h11.25L16 8.75l.66-.75l4.5 4.5l-4.5 4.5l-.66-.75L19.25 13H8z"/></svg>`;
 
     // 2b... add link and name to the items
     const logoutLink = [
-        {name: 'Logout', icon:logour_icon, link:'sign-in.html'},
+        {name: 'Logout', icon:logout_icon, link:'sign-in.html'},
     ];
     // 2b... add href to each items menu
     logoutLink.forEach(item => {
